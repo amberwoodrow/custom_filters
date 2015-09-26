3a0ce80c6b42dc4fcf7c56e71b0d0599cbf46bd3
amberwoodrow <amberwoodrow@gmail.com>
2015-09-26 15:21:10 -0600

    Custom filters complete

--- js/filters.js
+++ js/filters.js
@@ -15,12 +15,14 @@ app.filter('camelCase', function () {
   };
 });
 
-
-// function we(input) {
-//   var splitAtSpecified = input.split(/-|_|\s+/g);
-//   var camelArr = [];
-//   for (i=0; i<splitAtSpecified.length; i++) {
-//     i === 0 ? camelArr.push(splitAtSpecified[i].toLowerCase()) : camelArr.push(splitAtSpecified[i][0].toUpperCase()+ splitAtSpecified[i].slice(1));
-//   }
-//   return camelArr.join("");
-// }
+app.filter('redact', function () {
+  return function (string, word) {
+    strToArr = string.split(' ');
+    for(i=0; i<strToArr.length; i++){
+      if (strToArr[i] === word) {
+        strToArr[i] = 'REDACTED';
+      }
+    }
+    return strToArr.join(' ');
+  };
+});
